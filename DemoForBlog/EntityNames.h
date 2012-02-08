d8a533eb086470fdf028316549d7dae4f20fe70c
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2012-02-08 15:37:17 +0100

    Added Constants Definitions for entities.

---
+++ DemoForBlog/EntityNames.h
@@ -0,0 +1,13 @@
+//
+//  XmlNodeNames.h
+//  Endeavour Dashboard
+//
+//  Created by Jeroen Leenarts on 13-10-10.
+//  Copyright (c) 2010 Info Support. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const DEMO_ENTITY_NAME;
+extern NSString * const GRAPH_DATA_ENTITY_NAME;
+extern NSString * const SYNC_ENTITY_NAME;
---
+++ DemoForBlog/EntityNames.m
@@ -0,0 +1,16 @@
+//
+//  XmlNodeNames.m
+//  Endeavour Dashboard
+//
+//  Created by Jeroen Leenarts on 13-10-10.
+//  Copyright (c) 2010 Info Support. All rights reserved.
+//
+
+#import "EntityNames.h"
+
+NSString * const DEMO_ENTITY_NAME          = @"Demo";
+NSString * const GRAPH_DATA_ENTITY_NAME       = @"GraphData";
+NSString * const SYNC_ENTITY_NAME             = @"Sync";
+
+
+
