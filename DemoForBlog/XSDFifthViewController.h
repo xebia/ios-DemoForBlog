ad1dc6bfcc39e2e4bcd663c99394e91b8df0ceba
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2012-05-07 22:10:07 +0200

    Added fitth VC.

---
+++ DemoForBlog/XSDFifthViewController.h
@@ -0,0 +1,13 @@
+//
+//  XSDFifthViewController.h
+//  DemoForBlog
+//
+//  Created by Jeroen Leenarts on 07-05-12.
+//  Copyright (c) 2012 Xebia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XSDFifthViewController : UIViewController
+
+@end
---
+++ DemoForBlog/XSDFifthViewController.m
@@ -0,0 +1,43 @@
+//
+//  XSDFifthViewController.m
+//  DemoForBlog
+//
+//  Created by Jeroen Leenarts on 07-05-12.
+//  Copyright (c) 2012 Xebia. All rights reserved.
+//
+
+#import "XSDFifthViewController.h"
+
+@interface XSDFifthViewController ()
+
+@end
+
+@implementation XSDFifthViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
